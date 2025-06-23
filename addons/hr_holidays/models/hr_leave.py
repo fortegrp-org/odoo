6f8dee34ce77adf4c6dd97e3c394c16473e8b9e8
Vladislav Kakovko <vladislav.k@ventro.tech>
2025-06-23 21:12:33 +0200

    [REM] ODO-504: remove check leave_manager_id in write method hr.leave

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -971,12 +971,6 @@ Attempting to double-book your time off won't magically make your vacation 2x be
         if 'active' in values and not self.env.context.get('from_cancel_wizard'):
             raise UserError(_("You can't manually archive/unarchive a time off."))
 
-        is_officer = self.env.user.has_group('hr_holidays.group_hr_holidays_user') or self.env.is_superuser()
-        if not is_officer and values.keys() - {'attachment_ids', 'supported_attachment_ids', 'message_main_attachment_id'}:
-            if any(hol.date_from.date() < fields.Date.today() and hol.employee_id.leave_manager_id != self.env.user
-                   and hol.state not in ('confirm', 'draft') for hol in self):
-                raise UserError(_('You must have manager rights to modify/validate a time off that already begun'))
-
         # Unlink existing resource.calendar.leaves for validated time off
         if 'state' in values and values['state'] != 'validate':
             validated_leaves = self.filtered(lambda l: l.state == 'validate')
